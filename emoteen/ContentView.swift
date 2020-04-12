8eabfb563189c69c2b115934adf50a418d1ffb85
lananpurdy <59407846+lananpurdy@users.noreply.github.com>
2020-04-11 22:23:36 -0700

    read and write welcome

--- emoteen/ContentView.swift
+++ emoteen/ContentView.swift
@@ -16,13 +16,44 @@ func getTestJournal() -> [Journal] {
     
     let journal = Journal()
     journal.Title = "Welcome :(:"
-    journal.Body = "Body"
+    journal.Body = """
+    # emoteen
+    teens meditate on emotive states
+
+    ## ios app
+    ### mediations: ig stories / snaps of useful meditations. by teens, for teens, for free.
+    ### journals: your stories. private to you. or not.
+    ### mirrors: your stats, your friends, your teachers,
+
+    join us :): or not.
+    """
     
     //let journal2 = getJournalsForFiles()
+    
+    let file = getDocumentsDirectory().appendingPathComponent("welcome.emo")
+    
+    try! journal.Body.write(to: file, atomically: true, encoding: .utf8)
+    let input = try! String(contentsOf: file)
+    print(input)
+    
+    print(try! getDocuments())
     
     return [journal]
 }
 
+
+func getDocuments() throws -> [String]
+{
+    return try! FileManager.default.contentsOfDirectory(atPath: getDocumentsDirectory().path)
+}
+
+func getDocumentsDirectory() -> URL {
+    // find all possible documents directories for this user
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+    // just send back the first one, which ought to be the only one
+    return paths[0]
+}
 
 let testMeditation = getTestMeditations()
 
