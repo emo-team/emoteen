8d104f9f8a6e863222ab43ccd7824e62310c756d
dpurdy <dpurdy@purdy.im>
2020-06-13 23:26:16 -0700

    shim before UIKit + SwiftUI

--- emoteen/Activity.swift
+++ emoteen/Activity.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CalendarKit
 
 struct ActivityView: View {
     var body: some View {
@@ -20,5 +21,40 @@ struct ActivityView: View {
 struct Activity_Previews: PreviewProvider {
     static var previews: some View {
         ActivityView()
+    }
+}
+
+
+final class  CalendarController: UIViewControllerRepresentable, EventDataSource {
+    
+    typealias UIViewControllerType = DayViewController
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> DayViewController {
+        let imagePickerController = DayViewController()
+        imagePickerController.dataSource = self
+        return imagePickerController
+    }
+    
+    func eventsForDate(_ date: Date)  -> [EventDescriptor] {
+        var events = [Event]()
+        return events
+    }
+        
+    func updateUIViewController(_ uiViewController: DayViewController, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject {
+        var parent: CalendarController
+        
+        init(_ imagePickerController: CalendarController) {
+            self.parent = imagePickerController
+        }
+        
+        
     }
 }
